7154ab8d297db6a00e24b7e04e15eb443e2a5761
Carolina Mascarenhas <carolina.mascarenhass96@hotmail.com>
2022-07-17 12:03:38 -0300

    configuracoes do projeto

---
+++ exercicios/src/exerc-1.ts
@@ -0,0 +1,1 @@
+// Exercício 1: Vamos modelar algumas partes de um software de uma escola. Escreva uma classe cujos objetos representarão pessoas estudantes matriculadas em uma disciplina. Cada objeto dessa classe deve conter os seguintes dados: matrícula, nome, 4 notas de prova, 2 notas de trabalho.
